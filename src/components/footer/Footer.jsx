6f9233b16cdd94d67c003a21c13933e32de4b636
Ivan Bodnarash <bodnarash1999@gmail.com>
2026-04-18 19:58:29 +0200

    feat(footer): add email for the feedback

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="p-8 text-center">© Nahlah Almutawa. All rights reserved.</footer>
+    <footer className="p-8 text-center flex flex-col justify-center gap-4 ">
+      <div className="flex justify-center gap-4">
+        <p>Give us your feedback!</p>
+        <p className="underline hover:text-gray-700 transition-all">
+          <a href="mailto:PSlate.feedback@gmail.com">PSlate.feedback@gmail.com</a>
+        </p>
+      </div>
+      <div>© Nahlah Almutawa. All rights reserved.</div>
+    </footer>
   )
 }
